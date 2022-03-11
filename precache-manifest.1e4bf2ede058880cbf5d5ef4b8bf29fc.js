3920d9f0194b6b9d576bab3da7b297ddc77c6ca4
Hazel Wu <tinazx056@gmail.com>
2022-03-11 22:42:08 +0800

    Updates

---
+++ precache-manifest.1e4bf2ede058880cbf5d5ef4b8bf29fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "584cdcced1516255b6bc",
+    "url": "/todolist-solidity/static/css/main.667630a2.chunk.css"
+  },
+  {
+    "revision": "584cdcced1516255b6bc",
+    "url": "/todolist-solidity/static/js/main.584cdcce.chunk.js"
+  },
+  {
+    "revision": "5d454fef00ae79c9226e",
+    "url": "/todolist-solidity/static/js/1.5d454fef.chunk.js"
+  },
+  {
+    "revision": "dee34da9ad1844b09813",
+    "url": "/todolist-solidity/static/css/2.9ed1929c.chunk.css"
+  },
+  {
+    "revision": "dee34da9ad1844b09813",
+    "url": "/todolist-solidity/static/js/2.dee34da9.chunk.js"
+  },
+  {
+    "revision": "9dcc4cbf31e5fdcf0d9f",
+    "url": "/todolist-solidity/static/js/runtime~main.9dcc4cbf.js"
+  },
+  {
+    "revision": "693e0f4d4cda1433728b3fb96eb4e86d",
+    "url": "/todolist-solidity/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c2a63cf97841f8720d4be0f392727b40.js"
+  "/todolist-solidity/precache-manifest.1e4bf2ede058880cbf5d5ef4b8bf29fc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/todolist-solidity/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
