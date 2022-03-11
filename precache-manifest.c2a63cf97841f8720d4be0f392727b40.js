89c495ecd58170a00107d7794a91805a88f11229
Hazel Wu <tinazx056@gmail.com>
2022-03-11 22:33:21 +0800

    Updates

---
+++ precache-manifest.c2a63cf97841f8720d4be0f392727b40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "584cdcced1516255b6bc",
+    "url": "/static/css/main.667630a2.chunk.css"
+  },
+  {
+    "revision": "584cdcced1516255b6bc",
+    "url": "/static/js/main.584cdcce.chunk.js"
+  },
+  {
+    "revision": "5d454fef00ae79c9226e",
+    "url": "/static/js/1.5d454fef.chunk.js"
+  },
+  {
+    "revision": "f30271c945d1fbe425af",
+    "url": "/static/css/2.9ed1929c.chunk.css"
+  },
+  {
+    "revision": "f30271c945d1fbe425af",
+    "url": "/static/js/2.f30271c9.chunk.js"
+  },
+  {
+    "revision": "d100849422ee28fd18cc",
+    "url": "/static/js/runtime~main.d1008494.js"
+  },
+  {
+    "revision": "8373e99dfc56b6f416ec2145cec8423b",
+    "url": "/index.html"
+  }
+];
